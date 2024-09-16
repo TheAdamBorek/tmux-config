94a8eeb06cfe667a06608809156fb9d091c920d3
Adam Borek <adam@attio.com>
2024-09-16 14:31:17 +0200

    make terminal-swap smarter

--- terminal-swap.sh
+++ terminal-swap.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
-top_position=$(tmux display-message -p "#{pane_top}")
-panes_above=$(tmux list-panes -F "#{pane_top}" | awk -v top=$top_position '$1 < top')
-
-if [ ! -z "$panes_above" ]; then 
-    tmux select-pane -U
-    tmux resize-pane -Z
+terminal_pane_name='terminal'
+terminal_pane_index=$( tmux lsp -F 'Index: #P Title: #T' | (rg $terminal_pane_name || echo "-1") | awk '{if ($2 ~ /^[0-9]+$/) print $2; else print "-1"}')
+current_pane_index=$( tmux display-message -p '#P' )
+if [ $terminal_pane_index -eq -1 ]; then
+    tmux split-window -v -l 33%
+    tmux select-pane -T $terminal_pane_name
+    exit 0
 fi
 
+if [ $current_pane_index -eq $terminal_pane_index ]; then
+    tmux select-pane -U
+    tmux resize-pane -Z
+    exit 0
+fi
+
+tmux select-pane -t $terminal_pane_index
