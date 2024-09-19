8ac66bb08aebad5d0f3cbae4a88b4cf3e4c89c96
Adam Borek <adam@attio.com>
2024-09-19 10:24:25 +0200

    zoom current pane if terminal is opened already

--- terminal-swap.sh
+++ terminal-swap.sh
@@ -2,6 +2,8 @@
 terminal_pane_name='terminal'
 terminal_pane_index=$( tmux lsp -F 'Index: #P Title: #T' | (rg $terminal_pane_name || echo "-1") | awk '{if ($2 ~ /^[0-9]+$/) print $2; else print "-1"}')
 current_pane_index=$( tmux display-message -p '#P' )
+current_pane_zoomed=$( tmux display-message -p '#{?pane_zoomed,1,0}' )
+
 if [ $terminal_pane_index -eq -1 ]; then
     tmux split-window -v -l 33%
     tmux select-pane -T $terminal_pane_name
@@ -10,6 +12,11 @@ fi
 
 if [ $current_pane_index -eq $terminal_pane_index ]; then
     tmux select-pane -U
+    tmux resize-pane -Z
+    exit 0
+fi
+
+if [ $current_pane_zoomed -eq 0 ]; then
     tmux resize-pane -Z
     exit 0
 fi
