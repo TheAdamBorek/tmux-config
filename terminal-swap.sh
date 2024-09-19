6a08b41b3289798d390f1938144051fe8148d029
Adam Borek <adam@attio.com>
2024-09-19 13:07:36 +0200

    improve terminal-swap

--- terminal-swap.sh
+++ terminal-swap.sh
@@ -2,7 +2,6 @@
 terminal_pane_name='terminal'
 terminal_pane_index=$( tmux lsp -F 'Index: #P Title: #T' | (rg $terminal_pane_name || echo "-1") | awk '{if ($2 ~ /^[0-9]+$/) print $2; else print "-1"}')
 current_pane_index=$( tmux display-message -p '#P' )
-current_pane_zoomed=$( tmux display-message -p '#{?pane_zoomed,1,0}' )
 
 if [ $terminal_pane_index -eq -1 ]; then
     tmux split-window -v -l 33%
@@ -16,9 +15,11 @@ if [ $current_pane_index -eq $terminal_pane_index ]; then
     exit 0
 fi
 
-if [ $current_pane_zoomed -eq 0 ]; then
-    tmux resize-pane -Z
-    exit 0
-fi
+window_is_zoomed=$( tmux display-message -p '#{window_zoomed_flag}' )
 
-tmux select-pane -t $terminal_pane_index
+if [ $window_is_zoomed -eq 0 ]; then
+    tmux resize-pane -Z
+else
+    tmux resize-pane -Z
+    tmux select-pane -t $terminal_pane_index
+fi
